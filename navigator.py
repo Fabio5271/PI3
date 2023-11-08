f7efa29832e36499d0904ec69a64c6794df697b3
Fabio5271 <89141452+Fabio5271@users.noreply.github.com>
2023-11-08 18:04:18 -0300

    Implementação de uma busca longa no grafo (garantida)

--- navigator.py
+++ navigator.py
@@ -1,10 +1,11 @@
 from get_api import * # Funções da get_api.py
-from queue import Queue
 from collections import deque
 from PIL import Image, ImageDraw # Biblioteca que gera o GIF
+from sys import exit
 images = []
 
 pos = int
+start = int
 end_reached = bool
 avail_mov = list # lista de adj
 visited = [] # lista de visitados
@@ -21,10 +22,17 @@ def move(next_pos, silent = False): # Faz o movimento na api e atualiza os valor
     resp_movmt = post_movmt(grpid,labrt,next_pos) # faz o mov na api e guarda o retorno no dicionário 'resp_movmt'
     if silent is False:
         print(resp_movmt)
-    pos = resp_movmt['pos_atual']
-    end_reached = resp_movmt['final']
-    avail_mov = resp_movmt['movimentos']
-
+    try:
+        pos = resp_movmt['pos_atual']
+        end_reached = resp_movmt['final']
+        avail_mov = resp_movmt['movimentos']
+    except TypeError:
+        print("Erro: {0}".format(resp_movmt))
+        print('parent: {0}'.format(parent))
+        exit(1)
+    if end_reached is True:
+        print("\nAchamos o final!!!!!!!!\n")
+        exit(0)
 
 def traverse_node(next_pos):
     global grpid, labrt, pos, end_reached, avail_mov, dist_cntr, visited, queued, vert_q, dist, parent
@@ -52,25 +60,53 @@ def search_node(target):
     start_pos = pos
     for i in avail_mov:
         print('looking into {0}'.format(i))
-        move(i)
+        move(i, True)
         print("avail_mov: {0}".format(avail_mov))
         dist_cntr += 1 # aumenta a dist
         
         if target in avail_mov:
             print('found {0}!\n'.format(target))
-            break
+            return
 
         if parent[target] in avail_mov:
             print('found parent of {0}!'.format(target))
             move(parent[target])
             print()
             dist_cntr += 1
-            break
+            return
         
         print("not found, going back to {0}".format(start_pos))
         move(start_pos)
         print("avail_mov: {0}\n".format(avail_mov))
-        dist_cntr -= 1   
+        dist_cntr -= 1
+    # se ainda não encontramos target:
+    long_search_node(target)
+
+
+def long_search_node(target):
+    global grpid, labrt, pos, end_reached, avail_mov, dist_cntr, visited, queued, vert_q, dist, parent, start
+    print('long searching for: {0}'.format(target))
+    pos_ancst = []
+    targ_ancst = []
+    x = parent[pos]
+    y = parent[target]
+    while x is not start:
+        pos_ancst.append(x)
+        x = parent[x]
+    while y is not start:
+        targ_ancst.append(y)
+        y = parent[y]
+    pos_ancst.append(x)
+    targ_ancst.append(y)
+
+    for i in pos_ancst:
+        if i in targ_ancst:
+            while pos is not i:
+                move(parent[pos])
+            j = targ_ancst.index(i)
+            while pos is not parent[target]:
+                j -= 1
+                move(targ_ancst[j])
 
 
 def bfs():
@@ -109,6 +145,7 @@ resp_iniciar = post_iniciar(grpid, labrt)
 print(resp_iniciar)
 
 pos = resp_iniciar['pos_atual']
+start = pos
 end_reached = resp_iniciar['final']
 avail_mov = resp_iniciar['movimentos'] # lista de adj
        
